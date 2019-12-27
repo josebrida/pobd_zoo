e090ce4a92ced661a8ae98303dd863ae54bc87bd
josebrida <46499300+josebrida@users.noreply.github.com>
2019-12-27 17:57:37 +0000

    teste
    teste C++

---
+++ teste/teste/teste.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include <stdio.h> // for using printf
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	Console::WriteLine(L"Hello World");
+	printf("Hello World! \n");
+	return 0;
+}
